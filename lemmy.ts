74a4bfbb0bf0e65f6bf65c731494b4a177e80b88
Roland Oldengarm <roland@oldengarm.com>
2023-06-19 16:50:10 +1200

    feat: initial commit

---
+++ lemmy.ts
@@ -0,0 +1,48 @@
+import * as fs from "fs";
+import LemmyBot, { BotActions } from "lemmy-bot";
+import { env } from "process";
+import * as yaml from "yaml";
+const bot = new LemmyBot({
+  instance: "lemmy.world",
+  credentials: {
+    username: env.BOT_USERNAME,
+    password: env.BOT_PASSWORD,
+  },
+  dbFile: "db.sqlite3",
+  connection: {
+    minutesBeforeRetryConnection: 1,
+  },
+  schedule: [
+    {
+      runAtStart: false,
+      cronExpression: "0 9 * * *",
+      timezone: "Pacific/Auckland",
+      doTask: async (botActions: BotActions) => {
+        console.log("Starting bot task");
+        const dailycheckinsFile = fs.readFileSync(
+          "./dailycheckins.yml",
+          "utf8"
+        );
+        const dailycheckins = yaml.parse(dailycheckinsFile);
+        const communityId = await botActions.getCommunityId({
+          instance: "lemmy.world",
+          name: "stopdrinking",
+        });
+        const currentDay = new Date().getDay();
+        const key = `day_${currentDay}`;
+        const post = dailycheckins[key];
+        const name = post["title"];
+        const body = post["body"];
+        botActions.createPost({
+          name,
+          communityId,
+          body,
+          nsfw: false,
+        });
+        return Promise.resolve();
+      },
+    },
+  ],
+});
+
+bot.start();
