0482f8257c81403f39d22b607c10a9ab345b78fe
Roland Oldengarm <roland@oldengarm.com>
2023-06-24 15:59:32 +1200

    feat: http server

--- lemmy.ts
+++ lemmy.ts
@@ -47,3 +47,15 @@ const bot = new LemmyBot({
 });
 
 bot.start();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: any, res: any) => {
+  res.send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
